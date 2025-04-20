19331fa6341bcbeada57312abddf791d1d894f3d
hsyoodev <hsyoodev@icloud.com>
2025-04-20 20:42:27 +0900

    #9 - 테스트 파일 이름 수정
    * Tests -> Test

--- src/test/java/com/fastcampus/board/FastcampusBoardApplicationTest.java
+++ src/test/java/com/fastcampus/board/FastcampusBoardApplicationTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FastcampusBoardApplicationTests {
+class FastcampusBoardApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
