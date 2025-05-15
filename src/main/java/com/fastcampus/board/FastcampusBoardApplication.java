1edc4837329e77cc9506d65fbecdb73897de56b5
hsyoodev <hsyoodev@icloud.com>
2025-05-15 22:16:27 +0900

    #15 - thymeleaf 3 decoupled logic 설정

--- src/main/java/com/fastcampus/board/FastcampusBoardApplication.java
+++ src/main/java/com/fastcampus/board/FastcampusBoardApplication.java
@@ -2,12 +2,14 @@ package com.fastcampus.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class FastcampusBoardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FastcampusBoardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusBoardApplication.class, args);
+    }
 
 }
